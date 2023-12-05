8a9c83a8ff6674ad61b12612f299519986c35172
Abhinav Cillanki <abhinavcillanki@gmail.com>
2023-12-05 15:34:52 +0530

    01-js easy assignment solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,30 @@
 
 function isAnagram(str1, str2) {
 
+  // if length of str1 and str2 are not equal, return false
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  // convert both strings to lowercase
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  // create a character map for str1
+  let charMap1 = {};
+  for (let char of str1) {
+    charMap1[char] = charMap1[char] + 1 || 1;
+  }
+  // keep subtracting the count of characters of str2 from charMap1
+  for (let char of str2) {
+    if (!charMap1[char]) {
+      return false;
+    }
+    charMap1[char]--;
+    if (charMap1[char] < 0) {
+      return false;
+    }
+  }
+  return true;
+
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,26 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  list = [];
+  for (let transaction of transactions) {
+    let category = transaction.category;
+    let price = transaction.price;
+    let flag = false;
+    for (let obj of list) {
+      if (obj["category"] == category) {
+        obj["totalSpent"] += price;
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      let obj = {};
+      obj["category"] = category;
+      obj["totalSpent"] = price;
+      list.push(obj);
+    }
+  }
+  return list;
 }
 
 module.exports = calculateTotalSpentByCategory;
